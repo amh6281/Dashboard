c74c724d6137c803f1c9a74d8003c044ac624339
amh6281 <amh6281@naver.com>
2023-07-18 11:46:28 +0900

    PieChartBox 생성 2023.07.18
    기기별 사용자의 data 추가 후 props로 data를 전달받아 데이터를 렌더링한다.

--- src/data.ts
+++ src/data.ts
@@ -268,3 +268,10 @@ export const barChartBoxRevenue = {
     { name: "Sat", profit: 3490 },
   ],
 };
+
+export const pieChartBoxDevice = [
+  { name: "Mobile", value: 400, color: "#0088FE" },
+  { name: "Desktop", value: 300, color: "#00C49F" },
+  { name: "Laptop", value: 300, color: "#FFBB28" },
+  { name: "Tablet", value: 200, color: "#FF8042" },
+];
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,5 +1,6 @@
 import BarChartBox from "../../components/barChartBox/BarChartBox";
 import ChartBox from "../../components/chartBox/ChartBox";
+import PieChartBox from "../../components/pieChartBox/PieChartBox";
 import TopBox from "../../components/topBox/TopBox";
 import {
   barChartBoxRevenue,
@@ -8,6 +9,7 @@ import {
   chartBoxProduct,
   chartBoxRevenue,
   chartBoxUser,
+  pieChartBoxDevice,
 } from "../../data";
 import "./home.scss";
 
@@ -24,7 +26,9 @@ const Home = () => {
       <div className="box box3">
         <ChartBox {...chartBoxProduct} />
       </div>
-      <div className="box box4">Box4</div>
+      <div className="box box4">
+        <PieChartBox data={pieChartBoxDevice} />
+      </div>
       <div className="box box5">
         <ChartBox {...chartBoxConversion} />
       </div>
