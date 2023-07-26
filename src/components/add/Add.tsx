b999a03d71419a74d78ec6ce1534d46caa7d7ba5
amh6281 <amh6281@naver.com>
2023-07-26 12:57:18 +0900

    Add component 생성 및 props type 지정 2023.07.26

---
+++ src/components/add/Add.tsx
@@ -0,0 +1,13 @@
+import { GridColDef } from "@mui/x-data-grid";
+
+type Props = {
+  slug: string;
+  columns: GridColDef[];
+  setOpen: React.Dispatch<React.SetStateAction<boolean>>;
+};
+
+const Add = (props: Props) => {
+  return <div className="add">Add</div>;
+};
+
+export default Add;
